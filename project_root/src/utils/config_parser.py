ff35d0d613ea1acf3560940b364e8380316c3604
rosshan-orz <b23020028@njupt.edu.cn>
2026-01-23 18:34:38 +0800

    add config_parser.py to load and parse config YAML file

---
+++ project_root/src/utils/config_parser.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from typing import Union
+from box import Box
+import yaml
+
+class ConfigParser:
+    """
+    Loading and parsing YAML files to configuration
+    """
+
+    @staticmethod
+    def load(path: Union[str, Path]) -> Box:
+        """
+        Load a YAML configuration file from configs directory given by a Union and return the list to Box object.
+        This Box object will be frozen and will not allow change auto.
+        :param path: The path of the YAML config file
+        :return: Box object
+        """
+        path = Path(path)
+        if not path.exists():
+            raise FileNotFoundError(f"Config file {path} does not exist.")
+
+        with open(path, 'r', encoding='utf-8') as f:
+            config_dict = yaml.safe_load(f)
+
+        return Box(config_dict, frozen_box=True, default_box=False)
