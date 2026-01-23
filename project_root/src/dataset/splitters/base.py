d99650cbe810414a6ce9172213c7f2d75441fd65
rosshan-orz <b23020028@njupt.edu.cn>
2026-01-23 18:04:30 +0800

    Define abstract __call__ method in BaseSplitter and fix Tuple import

--- project_root/src/dataset/splitters/base.py
+++ project_root/src/dataset/splitters/base.py
@@ -1,8 +1,18 @@
 from abc import ABC, abstractmethod
-from typing import tuple
+from typing import Tuple
 from torch.utils.data import Dataset
 
 class BaseSplitter(ABC):
     """
     Abstract base class for dataset splitters
+    Key interface for splitting dataset to train and validation and test datasets
     """
+
+    @abstractmethod
+    def __call__(self, dataset: Dataset) -> Tuple[Dataset, Dataset, Dataset]:
+        """
+        Base class for dataset splitters
+        :param dataset: torch.utils.data.Dataset, dataset to be splitted
+        :return: tuple of train, validation and test datasets
+        """
+        raise NotImplementedError
